da0a89968b22eb602ab0720a03d3a42076165dca
Bryan Fichera <bfichera@anl.gov>
2026-07-07 11:35:44 -0500

    Add support for remote profiles

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,3 +7,19 @@ packages = '{{ cookiecutter.install_packages }}'.split()
 if packages:
     run([str(venv_dest_path / 'bin' / 'python'), '-m', 'pip', 'install'] +
         packages)
+
+remote_choice = '{{ cookiecutter.remote_profile }}'
+snakefile_path = Path('workflow') / 'Snakefile'
+if remote_choice == 'none':
+    run([
+        'mv',
+        str(Path('workflow') / 'Snakefile-local'),
+        str(snakefile_path),
+    ])
+elif remote_choice == 'carbon':
+    run([
+        'mv',
+        str(Path('workflow') / 'Snakefile-remote-carbon'),
+        str(snakefile_path)
+    ])
+run(['rm', str(Path('workflow') / 'Snakefile-*')])
