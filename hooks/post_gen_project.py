7bde79ba00c21f07e0ba426e7bbff2f49738ec5b
Bryan Fichera <bfichera@anl.gov>
2026-07-07 11:22:03 -0500

    Fix bugs in hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,11 @@
+from ast import literal_eval
 from pathlib import Path
 from subprocess import run
 
 project_name = '{{ cookiecutter.project_name }}'
-venv_dest_path = Path(project_name / 'workflow' / 'envs' / 'venv')
-run(['/usr/bin/env', 'python', '-m', 'venv', venv_dest_path])
-packages = '{{ cookiecutter.install_packages }}'
-run([venv_dest_path / 'bin' / 'python', '-m', 'pip', 'install'] + packages)
+venv_dest_path = Path(project_name) / 'workflow' / 'envs' / 'venv'
+run(['/usr/bin/env', 'python', '-m', 'venv', str(venv_dest_path)])
+packages = literal_eval('{{ cookiecutter.install_packages }}')
+if packages:
+    run([str(venv_dest_path / 'bin' / 'python'), '-m', 'pip', 'install'] +
+        packages)
