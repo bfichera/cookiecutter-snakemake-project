107bf1b304050f0f4b7458e02c766deb187d3362
Bryan Fichera <bfichera@anl.gov>
2026-07-07 11:23:56 -0500

    Switch empty list default to empty string

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,10 @@
-from ast import literal_eval
 from pathlib import Path
 from subprocess import run
 
 project_name = '{{ cookiecutter.project_name }}'
 venv_dest_path = Path(project_name) / 'workflow' / 'envs' / 'venv'
 run(['/usr/bin/env', 'python', '-m', 'venv', str(venv_dest_path)])
-packages = literal_eval('{{ cookiecutter.install_packages }}')
+packages = '{{ cookiecutter.install_packages }}'.split()
 if packages:
     run([str(venv_dest_path / 'bin' / 'python'), '-m', 'pip', 'install'] +
         packages)
