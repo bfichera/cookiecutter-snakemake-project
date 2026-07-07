f475990649558df525426ea4b422383cc1ab2a48
Bryan Fichera <bfichera@anl.gov>
2026-07-07 11:25:28 -0500

    Fix venv location

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from subprocess import run
 
-project_name = '{{ cookiecutter.project_name }}'
-venv_dest_path = Path(project_name) / 'workflow' / 'envs' / 'venv'
+venv_dest_path = Path('workflow') / 'envs' / 'venv'
 run(['/usr/bin/env', 'python', '-m', 'venv', str(venv_dest_path)])
 packages = '{{ cookiecutter.install_packages }}'.split()
 if packages:
